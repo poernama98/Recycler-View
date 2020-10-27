ce7b60f030c1b18d9b521a9e5c0ec0b71a7434e5
unknown <rizky.purnama@students.amikom.ac.id>
2020-10-27 13:07:53 +0700

    Recycler View

---
+++ app/src/main/java/com/example/PCS_RecyclerView_0110/Movie.java
@@ -0,0 +1,27 @@
+package com.example.PCS_RecyclerView_0110;
+
+public class Movie {
+    public String title;
+    public String year;
+
+    public Movie(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
